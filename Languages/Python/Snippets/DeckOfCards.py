f9c229fb5753a5ede5e0519fe4a015f2d678a9a3
David Pedrelli <programming@ibis-nt.com>
2022-05-17 13:35:25 -0400

    Added Python snippets

---
+++ Languages/Python/Snippets/DeckOfCards.py
@@ -0,0 +1,26 @@
+﻿from random import shuffle
+
+class Deck:
+    def __init__(self):
+        from itertools import product
+        values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K" ]
+        suits = ["♠", "♣", "♥", "♦"]
+        self.list_of_cards = []
+        list(map(
+            lambda v: self.list_of_cards.append(Card(*v)),
+            list(product(values, suits))
+        ))
+
+    def shuffle(self): shuffle(self.list_of_cards)
+
+    def show(self): [ print(card, end=r"    ") for card in self.list_of_cards ]
+
+class Card:
+    def __init__(self, value, suit): self.name = f"{value} {suit}"
+
+    def __str__(self): return self.name
+
+if __name__ == "__main__":
+    deck = Deck()
+    deck.shuffle()
+    deck.show()
---
+++ Languages/Python/Snippets/Fibonacci.py
@@ -0,0 +1,25 @@
+def fibonacci(number, display = False, returnAll = False):
+    a = 0
+    b = 1
+    fib = 0
+    result = [0, 1]
+    if ((display) & (number >= 0)):
+        print(a)
+    if ((display) & (number >= 1)):
+        print(b)
+    for x in range(number - 1):
+        fib = a + b
+        if (display):
+            print(fib)
+        if (returnAll):
+            result.append(fib)
+        a = b
+        b = fib
+    if (returnAll):
+        return result
+    else:
+        return [fib]
+
+result = fibonacci(100, False, True)
+for x in range(len(result)):
+    print(result[x])
